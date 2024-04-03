9ef18de69816d6dc679c6b6760c9e4e4c7360e31
Muhammadkhalid Alfaj Mujawar <124422255+khalidmujawar07@users.noreply.github.com>
2024-04-03 18:09:27 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Header from './components/Header.vue'
+// import Card from './components/card.vue'
+import View from './components/View.vue'
+import Footer from './components/Footer.vue'
+
+</script>
+
+<template>
+  <Header/>
+  <View/>
+  <!-- <Card/> -->
+  <Footer/>
+</template>
+
+<style scoped>
+
+
+</style>
